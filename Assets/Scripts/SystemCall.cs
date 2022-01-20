197b7cb8706928879977e76e1b5d70e052c848f7
alperyasar <alperyasar@gtu.edu.tr>
2022-01-20 13:23:57 +0300

    Update

--- Assets/Scripts/SystemCall.cs
+++ Assets/Scripts/SystemCall.cs
@@ -35,6 +35,8 @@ public class SystemCall : MonoBehaviour
     bool timeChanged = true;
     int mapScale = 1;
     private int i =0,stationList=-1;
+    private int[] randomRatio = { 0, 0, 0, 0 };
+    private int[] randomRatio1 = { 0, 0, 0, 0 };
     float runTime = 24f, virtualTime = 0f;
 
     void Awake()
@@ -244,13 +246,119 @@ public class SystemCall : MonoBehaviour
         int randomNumberofPass = Random.Range(0, (int)mainmenu.passNumb);
         for(int j = 0; j< randomNumberofPass; j++)
         {
-            firstStation = Random.Range(0, lenghtBusLine-1);
-            secondStation = Random.Range(firstStation, lenghtBusLine);
+            /*   firstStation = Random.Range(0, lenghtBusLine - 1);
+               secondStation = Random.Range(firstStation, lenghtBusLine - 1);*/
+            firstStation = randomGenerator(lenghtBusLine);
+            while (firstStation < 0)
+            {
+                if(firstStation == -1) {
+                    firstStation = randomGenerator(30);
+                }
+                else if (firstStation == -2)
+                {
+                    firstStation = randomGenerator(20);
+                }
+                else
+                    firstStation = randomGenerator(10);
+            }
+            secondStation = randomGeneratorFinish(firstStation);
+            while (secondStation < 0)
+            {
+                   secondStation = randomGeneratorFinish(firstStation);                
+            }
             Passengers pass = new Passengers(buses.getStationName(firstStation), buses.getStationName(secondStation));
             buses.addPassengertoStation(firstStation, pass);
 
 
         }
+    }
+    private int randomGenerator(int lenght)
+    {
+        randomGenerateCheck();
+        int firstStation = Random.Range(0, lenght - 1);
+        if (firstStation > 30 && randomRatio[3] == 5)
+        {
+            return -1;
+        }
+        else if(firstStation > 30 && randomRatio[3] != 5)
+        {
+            randomRatio[3]++;
+            return firstStation;
+        }
+        else if (firstStation > 20 && firstStation <= 30 && randomRatio[2] == 15)
+        {
+            return -2;
+        }
+        else if (firstStation > 20 && firstStation <= 30 && randomRatio[2] != 15)
+        {
+            randomRatio[2]++;
+            return firstStation;
+        }
+        else if (firstStation > 10 && firstStation <= 20 && randomRatio[1] == 30)
+        {
+            return -3;
+        }
+        else if (firstStation > 10 && firstStation <= 20 && randomRatio[1] != 30)
+        {
+            randomRatio[1]++;
+            return firstStation;
+        }
+        else
+            randomRatio[0]++;
+        return firstStation;
+    }
+    private void randomGenerateCheck()
+    {
+        int sum = 0;
+        foreach (int j in randomRatio)
+            sum += j;
+        if (sum == 100)
+            for (int j = 0; j < randomRatio.Length; j++)
+                randomRatio[j] = 0;
+    }
+    private int randomGeneratorFinish(int firstStation)
+    {
+        randomGenerateCheck1();
+        int secondStation = Random.Range(firstStation, buses.getlineLenght() - 1);
+        if (secondStation < 10 && randomRatio1[3] == 5)
+        {
+            return -1;
+        }
+        else if (secondStation < 10 && randomRatio1[3] != 5)
+        {
+            randomRatio1[3]++;
+            return secondStation;
+        }
+        else if (secondStation >= 10 && secondStation <= 20 && randomRatio1[2] == 15)
+        {
+            return -2;
+        }
+        else if (secondStation >= 10 && secondStation <= 20 && randomRatio1[2] != 15)
+        {
+            randomRatio1[2]++;
+            return secondStation;
+        }
+        else if (secondStation > 20 && secondStation <= 30 && randomRatio1[1] == 30)
+        {
+            return -3;
+        }
+        else if (secondStation > 20 && secondStation <= 30 && randomRatio1[1] != 30)
+        {
+            randomRatio1[1]++;
+            return secondStation;
+        }
+        else
+            randomRatio1[0]++;
+        return secondStation;
+    }
+    private void randomGenerateCheck1()
+    {
+        int sum = 0;
+        foreach (int j in randomRatio1)
+            sum += j;
+        if (sum == 100)
+            for (int j = 0; j < randomRatio1.Length; j++)
+                randomRatio1[j] = 0;
     }
     public void AccelerationChanged(TextMeshProUGUI text)
     {
@@ -311,29 +419,28 @@ public class SystemCall : MonoBehaviour
                             otherBusPass += buss[busses[k]].getTotalPassengers();
                         }
                     }
-                    if(buss[busNumber].getTotalPassengers() - (otherBusPass / busses.Count-1) > j)
+                    
+                    
+                    if(busses[0] == busNumber)
+                    {
+                        string str = passInStationNumber + " passenger in Station enter to bus : " + busNumber;
+                        StartCoroutine(busNoteToScreen(busses,buses.getStationName(stationNumber),str));
+                        Debug.Log("passengerNumber : " + passInStationNumber +" " + busses.Count + " enter to bus : " + busNumber);
+                        Passengers pass = buses.getInBus(stationNumber);
+                        showStation.transform.GetChild(j - 1).gameObject.SetActive(false);
+                        busColor = buss[busNumber].increasePassengers(pass);
+                        if (busColor == 0)
+                            clone[busNumber].GetComponent<SpriteRenderer>().sprite = GreenBus;
+                        else if (busColor == 1)
+                            clone[busNumber].GetComponent<SpriteRenderer>().sprite = YellowBus;
+                        else if (busColor == 2)
+                            clone[busNumber].GetComponent<SpriteRenderer>().sprite = redBus;
+                    }
+                        
+                    yield return new WaitForSeconds(3f / acceleration);
+                    if (buss[busNumber].getTotalPassengers() - (otherBusPass / busses.Count - 1) > j)
                     {
                         j = 0;
-                    }
-                    else
-                    {
-                        if(busses[0] == busNumber)
-                        {
-                            string str = passInStationNumber + "passenger in Station enter to bus : " + busNumber;
-                            StartCoroutine(busNoteToScreen(busses,buses.getStationName(stationNumber),str));
-                            Debug.Log("passengerNumber : " + passInStationNumber +" " + busses.Count + " enter to bus : " + busNumber);
-                            Passengers pass = buses.getInBus(stationNumber);
-                            showStation.transform.GetChild(j - 1).gameObject.SetActive(false);
-                            busColor = buss[busNumber].increasePassengers(pass);
-                            if (busColor == 0)
-                                clone[busNumber].GetComponent<SpriteRenderer>().sprite = GreenBus;
-                            else if (busColor == 1)
-                                clone[busNumber].GetComponent<SpriteRenderer>().sprite = YellowBus;
-                            else if (busColor == 2)
-                                clone[busNumber].GetComponent<SpriteRenderer>().sprite = redBus;
-                        }
-                        
-                        yield return new WaitForSeconds(3f / acceleration);
                     }
                 }
             }
